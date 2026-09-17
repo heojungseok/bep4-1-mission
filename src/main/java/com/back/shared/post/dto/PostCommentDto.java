6ba4ef176a3819ad761cab3e2c5b5ea37fb3b9e6
heojungseok <tjrwjdgj@gmail.com>
2026-09-17 14:28:20 +0900

    feat: [0007] 각 바운디드 컨텍스트 내부를 DDD 형태로 구조화

--- src/main/java/com/back/shared/post/dto/PostCommentDto.java
+++ src/main/java/com/back/shared/post/dto/PostCommentDto.java
@@ -1,7 +1,7 @@
 package com.back.shared.post.dto;
 
 
-import com.back.boundedContext.post.entity.PostComment;
+import com.back.boundedContext.post.domain.PostComment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
