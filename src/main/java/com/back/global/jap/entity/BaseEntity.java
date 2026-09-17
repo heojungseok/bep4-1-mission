48c9f8b87c0d6fdea903485ba9cb5c066bd62fd3
heojungseok <tjrwjdgj@gmail.com>
2026-09-17 12:56:45 +0900

    feat: [0005] com.back.boundedContext.member, com.back.boundedContext.post, com.back.global 패키지 도입(모듈 분리)

--- src/main/java/com/back/global/jap/entity/BaseEntity.java
+++ src/main/java/com/back/global/jap/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.back.jap.entity;
+package com.back.global.jap.entity;
 
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
--- src/main/java/com/back/global/jap/entity/BaseIdAndTime.java
+++ src/main/java/com/back/global/jap/entity/BaseIdAndTime.java
@@ -1,4 +1,4 @@
-package com.back.jap.entity;
+package com.back.global.jap.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
